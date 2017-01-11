a01b2b47ce877cd6387d3bdd977cf572df3dbfe6
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2017-01-11 09:43:00 +0700

    docs: expand cli ergonomics

--- src/json.rs
+++ src/json.rs
@@ -50,3 +50,23 @@ impl Json {
     }
 
     /// Borrow this value as a string, if it is a string.
+    pub fn as_str(&self) -> Option<&str> {
+        match self {
+            Json::Str(s) => Some(s),
+            _ => None,
+        }
+    }
+
+    /// Interpret this value as an `f64`, if it is a number.
+    pub fn as_f64(&self) -> Option<f64> {
+        match self {
+            Json::Num(n) => Some(*n),
+            _ => None,
+        }
+    }
+
+    /// Interpret this value as a bool, if it is a boolean.
+    pub fn as_bool(&self) -> Option<bool> {
+        match self {
+            Json::Bool(b) => Some(*b),
+            _ => None,
