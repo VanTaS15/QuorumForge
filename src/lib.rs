18c53b736d2423afd1e913157db1d40d839b6115
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2017-02-21 13:15:00 +0700

    refactor: tidy tolerance tuning

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! # QuorumForge
+//!
+//! QuorumForge is a **multi-agent evidence adjudication engine**. It ingests a
+//! recorded deliberation — a council of weighted agents taking positions on a
+//! set of claims, with citations and confidences — and turns it into a
+//! deterministic verdict: which claims reached consensus, which are contested,
