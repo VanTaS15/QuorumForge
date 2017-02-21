5ea15fd8d85c0a1f3fd4ff5226a0575c7030bd35
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2017-02-21 13:20:00 +0700

    perf: improve json codec

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,10 @@
 //! recorded deliberation — a council of weighted agents taking positions on a
 //! set of claims, with citations and confidences — and turns it into a
 //! deterministic verdict: which claims reached consensus, which are contested,
+//! which merely lean one way, and which lack the evidence to conclude anything.
+//!
+//! It is *not* an agent orchestrator. QuorumForge does not run agents, call
+//! models, or manage prompts. It operates purely on the *record* of a
+//! deliberation after the fact, which is what makes its judgements auditable
+//! and reproducible.
+//!
