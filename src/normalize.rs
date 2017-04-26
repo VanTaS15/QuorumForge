173611c86ad902d24024914898e7f87de16f2814
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2017-04-26 14:11:00 +0700

    refactor: report rendering

---
+++ src/normalize.rs
@@ -0,0 +1,18 @@
+//! Claim normalization.
+//!
+//! Two agents rarely phrase the same proposition identically. The normalizer
+//! reduces claim text to a canonical form so that near-duplicate claims can be
+//! recognised and, optionally, merged. Normalization is intentionally
+//! conservative: it lower-cases, collapses whitespace, strips a small set of
+//! leading hedges and trailing punctuation, and folds a handful of common
+//! contractions. It does *not* attempt semantic paraphrase detection — that
+//! would make verdicts non-deterministic and hard to audit.
+
+use crate::model::{Claim, Deliberation};
+use std::collections::BTreeMap;
+
+/// Hedging prefixes that carry no propositional content and are dropped from
+/// the head of a claim during normalization.
+const LEADING_HEDGES: &[&str] = &[
+    "i think that",
+    "i think",
