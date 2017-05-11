9ea9e7b5c2e1b3299b5856d41d436d5cf609d213
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2017-05-11 10:06:00 +0700

    style: clean up bundle format

--- src/normalize.rs
+++ src/normalize.rs
@@ -39,3 +39,14 @@ pub fn normalize_text(text: &str) -> String {
 
     // 2. Strip trailing sentence punctuation.
     while matches!(
+        joined.chars().last(),
+        Some('.') | Some('!') | Some('?') | Some(';')
+    ) {
+        joined.pop();
+    }
+    joined = joined.trim().to_string();
+
+    // 3. Drop a single leading hedge if present (longest match first).
+    for hedge in LEADING_HEDGES {
+        if let Some(rest) = joined.strip_prefix(hedge) {
+            // Require a word boundary so "likely" does not swallow "likelihood".
