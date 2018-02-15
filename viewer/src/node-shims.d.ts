abca97028aa08db76a8b4f2b2eefb0cc805bcf9a
VanTaS15 <22029244+VanTaS15@users.noreply.github.com>
2018-02-15 16:37:00 +0700

    docs: document html export

---
+++ viewer/src/node-shims.d.ts
@@ -0,0 +1,4 @@
+// Minimal ambient declarations for the tiny slice of the Node.js runtime the
+// viewer touches. QuorumForge's viewer is deliberately dependency-light: it
+// pulls in neither `@types/node` nor any runtime packages. These declarations
+// cover exactly what `cli.ts` uses and nothing more.
